4b581ac0d8d10b25de2344f2c234e0be62ba3c91
Ikbarsf <ikbarsaif@gmail.com>
2023-06-08 00:52:54 +0700

    update data

--- resources/views/customer/pages/course/list.blade.php
+++ resources/views/customer/pages/course/list.blade.php
@@ -55,7 +55,7 @@
                             {{-- <a class="d-block bgi-no-repeat bgi-size-cover bgi-position-center card-rounded position-relative min-h-175px mb-10" style="background-image:url('{{asset('image/upload/course/thumbnail')}}/{{$item->thumbnail_image}}')" data-fslightbox="lightbox-video-tutorials" href="{{$item->thumbnail_video}}">
                                 <img src="{{asset('vendor/media/svg/misc/video-play.svg')}}" class="position-absolute top-50 start-50 translate-middle" alt="" />
                             </a> --}}
-                            <img class="card-rounded min-h-175px mb-10" src="{{asset('image/upload/course/thumbnail')}}/{{$item->thumbnail_image}}"/>
+                            <img class="card-rounded min-h-175px mb-10 h-8" src="{{asset('image/upload/course/thumbnail')}}/{{$item->thumbnail_image}}"/>
                             <div class="m-0 h-250px">
                                 <a href="{{url('#')}}" class="fs-4 text-dark fw-bolder text-hover-primary text-dark lh-base">{{$item->course_name}}</a>
                                 <div class="fw-bold fs-5 text-gray-600 text-dark my-4">{{ \Illuminate\Support\Str::limit($item->harga, 150, $end='...') }}</div>
